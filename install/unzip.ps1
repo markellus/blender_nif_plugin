4a504754e64fee773f49d82579dc275ca6233a24
Markellus <gmx@marcel-bulla.de>
2019-03-16 03:44:28 -0700

    Don't kill my addon folder

--- install/unzip.ps1
+++ install/unzip.ps1
@@ -6,11 +6,7 @@ param (
 write ("Source : {0}" -f $source)
 write ("Destination : {0}" -f $destination)
 
-If(Test-path $destination) {
-write ("Ensuring existing folder is removed {0}" -f $destination)
-Remove-item $destination
-write ("Success")
-}
+
 
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipFile]::ExtractToDirectory($source, $destination)
